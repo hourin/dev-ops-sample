9292cb4e1826dca5352031bbbeb0a8a6ab180eb1
akihiko.kigure <akihiko.kigure@gmail.com>
2024-02-25 20:18:58 +0900

    get("/api/memos/{id}") 追加

--- backend/my_actix_app/src/main.rs
+++ backend/my_actix_app/src/main.rs
@@ -34,10 +34,29 @@ async fn get_memos() -> impl Responder {
     HttpResponse::Ok().json(memos)
 }
 
+#[get("/api/memos/{id}")]
+async fn get_memo_by_id(req: HttpRequest) -> impl Responder {
+    let id: i32 = req.match_info().get("id").unwrap().parse().unwrap();
+    let url = "mysql://ユーザー名:パスワード@ホスト名:3306/データベース名";
+    let pool = Pool::new(url).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+
+    let memo: Option<Memo> = conn
+        .exec_first("SELECT ID, TITLE, MEMO FROM MEMOS WHERE ID = :id", params! { "id" => id })
+        .unwrap_or(None)
+        .map(|row| Memo::from_row(row));
+
+    match memo {
+        Some(memo) => HttpResponse::Ok().json(memo),
+        None => HttpResponse::NotFound().body("Memo not found"),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new()
             .service(get_memos)
+            .service(get_memo_by_id)
         )
         .bind("0.0.0.0:8080")?
         .run()
