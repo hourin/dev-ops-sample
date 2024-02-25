3736977a79a249bc2b9acab60f11b74f3aec427b
akihiko.kigure <akihiko.kigure@gmail.com>
2024-02-25 20:13:50 +0900

    データベース用インターフェース実装

--- backend/my_actix_app/src/main.rs
+++ backend/my_actix_app/src/main.rs
@@ -3,6 +3,24 @@ use mysql::prelude::*;
 use mysql::*;
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct Memo {
+    id: Option<i32>,
+    title: String,
+    memo: String,
+}
+
+impl Memo {
+    fn from_row(row: mysql::Row) -> Memo {
+        let (id, title, memo): (Option<i32>, String, String) = (
+            row.get("ID").unwrap_or(None),
+            row.get("TITLE").unwrap_or(String::new()),
+            row.get("MEMO").unwrap_or(String::new()),
+        );
+        Memo { id, title, memo }
+    }
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
