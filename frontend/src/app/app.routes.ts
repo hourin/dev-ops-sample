1241b8235864b185cb39299a414883c5ad6e4df8
akihiko.kigure <akihiko.kigure@gmail.com>
2024-03-02 13:42:17 +0900

    ページ遷移用ルーター実装

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => import('./todo-list/todo-list.component'),
+  },
+  {
+    path: 'new',
+    pathMatch: 'full',
+    loadComponent: () => import('./todo/todo.component'),
+  },
+  {
+    path: 'edit/:id',
+    pathMatch: 'full',
+    loadComponent: () => import('./todo/todo.component'),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./page-not-found/page-not-found.component'),
+  },
+];
