3dd5ade080c83485c59f8a0a40c4069b00462ba5
akihiko.kigure <akihiko.kigure@gmail.com>
2024-03-02 18:39:10 +0900

    メモ帳一覧画面機能実装

--- frontend/src/app/todo-list/todo-list.component.ts
+++ frontend/src/app/todo-list/todo-list.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MemoUi } from '../shared/models/memo-ui';
+import { DUMMY_DATA } from '../dummy-data';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatCheckboxModule, MatTableModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
 })
-export default class TodoListComponent {}
+export default class TodoListComponent {
+  dummyData = DUMMY_DATA;
+  displayedColumns: string[] = ['checked', 'id', 'title', 'memo'];
+  dataSource = new MatTableDataSource<MemoUi>(this.dummyData);
+
+  router = inject(Router);
+
+  memoFilter = () =>
+    this.dummyData
+      .filter((data) => data.checked === true)
+      .map((data) => data.id);
+
+  /**
+   * 全選択処理
+   * @returns{void}
+   */
+  checkAll(): void {
+    this.dummyData.forEach((data) => (data.checked = true));
+  }
+
+  clearCheckAll(): void {
+    this.dummyData.forEach((data) => (data.checked = false));
+  }
+
+  deleteMemo(): void {
+    const deleteTarget = this.memoFilter();
+
+    if (deleteTarget.length === 0) {
+      window.alert('削除対象が選択されていません');
+      return;
+    }
+
+    if (window.confirm('削除します。よろしいですか？')) {
+      this.dummyData = this.dummyData.filter(
+        (item) => !deleteTarget.includes(item.id)
+      );
+      this.dataSource = new MatTableDataSource<MemoUi>(this.dummyData);
+    }
+  }
+
+  createMemo(): void {
+    this.router.navigate(['new']);
+  }
+
+  editMemo(): void {
+    const editTarget = this.memoFilter();
+    if (editTarget.length === 0) {
+      window.alert('編集対象が選択されていません');
+      return;
+    }
+
+    if (editTarget.length > 1) {
+      window.alert('編集対象がは一つです。');
+      return;
+    }
+
+    this.router.navigate(['edit', editTarget[0]]);
+  }
+}
